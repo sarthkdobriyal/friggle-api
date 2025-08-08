9cc59e3a136adbbd6ee78e15670d3a64d54d4ea4
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2025-08-08 15:07:15 +0530

    refactor: make allusers function asynchronous and include totalAdmins in stats

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,10 +1,13 @@
 const Video = require('../models/videoModel');
 const User = require('../models/userModel');
 
-const allusers = (req, res) => {
+const allusers = async (req, res) => {
     try {
-        console.log('Fetching all users', req.user);
-
+        // Fetch all users without password field
+        const users = await User.find().select('-password');
+        
+        console.log('Fetching all users:', users.length);
+        res.status(200).json(users);
     } catch (error) {
         console.error('Error fetching all users:', error);
         res.status(500).json({ error: 'Failed to fetch all users' });
@@ -17,9 +20,11 @@ const getStats = async (req, res) => {
         // Example stats data
         const totalUsers = await User.find().countDocuments();
         const totalVideos = await Video.countDocuments();
+        const totalAdmins = await User.countDocuments({ role: 'admin' });
         const stats = [
             { title: 'Total Users', value: totalUsers },
             { title: 'Total Videos', value: totalVideos },
+            { title: 'Total Admins', value: totalAdmins },
         ]
 
         console.log('Fetching stats:', stats);
