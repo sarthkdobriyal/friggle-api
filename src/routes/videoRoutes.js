cfb70eb967457de62833ba0bbad9c075a26c340e
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2025-10-14 16:43:34 +0530

    fix: comment out unused video route handlers in videoRoutes.js for cleanup

--- src/routes/videoRoutes.js
+++ src/routes/videoRoutes.js
@@ -2,20 +2,20 @@ const express = require('express');
 const { 
     // generateAiVideo, 
     getRecentVideos, 
-    getAllVideos, 
-    enhancePrompt, 
-    getExampleVideos 
+    // getAllVideos, 
+    // enhancePrompt, 
+    // getExampleVideos 
 } = require('../controllers/videoController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 
-router.get('/examples', getExampleVideos);
 // router.post('/generate', authMiddleware, generateAiVideo);
-router.post('/enhancePrompt', authMiddleware, enhancePrompt);
+// router.post('/enhancePrompt', authMiddleware, enhancePrompt);
 router.get('/recent', authMiddleware, getRecentVideos);
-router.get('/all', authMiddleware, getAllVideos);
+// router.get('/all', authMiddleware, getAllVideos);
+// router.get('/examples', getExampleVideos);
 
 
 module.exports = router;
