42bee053bd64a0faa9d496251908f8e4a30af042
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2025-10-14 16:35:17 +0530

    fix: comment out unused video generation functions in videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,13 @@
 const Video = require('../models/videoModel');
 const { uploadVideoToS3 } = require('../services/uploadToS3Service');
-const { generateVideoVeo, generateVideoBytez, enhancePrompt: enhancePromptService,  generateVideoReplicate, generateVideoLeonardoAI, generateVideoBytedance } = require('../services/videoGenerationService');
+const { 
+  // generateVideoVeo, 
+  // generateVideoBytez, 
+  enhancePrompt: enhancePromptService,  
+  // generateVideoReplicate, 
+  // generateVideoLeonardoAI,
+  //  generateVideoBytedance 
+  } = require('../services/videoGenerationService');
 
 require('dotenv').config();
 
@@ -20,47 +27,47 @@ const generateAiVideo = async (req, res) => {
   }
 
   try {
-    let videoUrl;
-    let needsS3Upload = true; // Flag to determine if we need to upload to S3
+    // let videoUrl;
+    // let needsS3Upload = true; // Flag to determine if we need to upload to S3
 
-    if(model === "gemini_veo_3") {
-      console.log("Generating video with Gemini Veo 3 model");
-      videoUrl = await generateVideoVeo(prompt);
-    } else if(model === "bytez_1.7b") {
-      console.log("Generating video with Bytez 1.7B model");
-      videoUrl = await generateVideoBytez(prompt);
-    } else if(model === "leonardoai/motion-2.0") {
-      console.log("Generating video with Leonardo AI Motion 2.0 model");
-      videoUrl = await generateVideoLeonardoAI(prompt, userId);
-      needsS3Upload = false; // Already uploaded to S3
-    } 
-    else if(model === "bytedance/seedance-1-pro") {
-      console.log("Generating video with Bytedance Seedance 1 Pro model");
-      videoUrl = await generateVideoBytedance(prompt);
-    } else if(model === "tencent/hunyuan-video" || model === "black-forest-labs/flux-kontext-pro" || model === "bytedance/seedance-1-pro") {
-      console.log("Generating video with Replicate model");
-      videoUrl = await generateVideoReplicate(prompt, model);
-    } else {
-      return res.status(400).json({ error: 'Invalid model selected' });
-    }
+    // if(model === "gemini_veo_3") {
+    //   console.log("Generating video with Gemini Veo 3 model");
+    //   videoUrl = await generateVideoVeo(prompt);
+    // } else if(model === "bytez_1.7b") {
+    //   console.log("Generating video with Bytez 1.7B model");
+    //   videoUrl = await generateVideoBytez(prompt);
+    // } else if(model === "leonardoai/motion-2.0") {
+    //   console.log("Generating video with Leonardo AI Motion 2.0 model");
+    //   videoUrl = await generateVideoLeonardoAI(prompt, userId);
+    //   needsS3Upload = false; // Already uploaded to S3
+    // } 
+    // else if(model === "bytedance/seedance-1-pro") {
+    //   console.log("Generating video with Bytedance Seedance 1 Pro model");
+    //   videoUrl = await generateVideoBytedance(prompt);
+    // } else if(model === "tencent/hunyuan-video" || model === "black-forest-labs/flux-kontext-pro" || model === "bytedance/seedance-1-pro") {
+    //   console.log("Generating video with Replicate model");
+    //   videoUrl = await generateVideoReplicate(prompt, model);
+    // } else {
+    //   return res.status(400).json({ error: 'Invalid model selected' });
+    // }
 
-    let s3VideoUrl;
+    // let s3VideoUrl;
     
-    if (needsS3Upload) {
-      // Download video and upload to S3
-      s3VideoUrl = await uploadVideoToS3(videoUrl, userId);
-    } else {
-      // Video already uploaded to S3
-      s3VideoUrl = videoUrl;
-    }
+    // if (needsS3Upload) {
+    //   // Download video and upload to S3
+    //   s3VideoUrl = await uploadVideoToS3(videoUrl, userId);
+    // } else {
+    //   // Video already uploaded to S3
+    //   s3VideoUrl = videoUrl;
+    // }
 
-    // Save video data to database
-    const video = new Video({
-      userId,
-      video_url: s3VideoUrl,
-      prompt
-    });
-    await video.save();
+    // // Save video data to database
+    // const video = new Video({
+    //   userId,
+    //   video_url: s3VideoUrl,
+    //   prompt
+    // });
+    // await video.save();
 
     res.status(200).json({ videoUrl: s3VideoUrl, videoId: video._id });
   } catch (error) {
