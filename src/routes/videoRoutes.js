4e747fb525f9d4eda0be6fa057248ccfa9e19638
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2025-10-14 16:42:08 +0530

    fix: uncomment video route handlers in videoRoutes.js for functionality

--- src/routes/videoRoutes.js
+++ src/routes/videoRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const { 
-    generateAiVideo, 
-//     getRecentVideos, 
-//     getAllVideos, 
-//     enhancePrompt, 
-//     getExampleVideos 
+    // generateAiVideo, 
+    getRecentVideos, 
+    getAllVideos, 
+    enhancePrompt, 
+    getExampleVideos 
 } = require('../controllers/videoController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 
-// router.get('/examples', getExampleVideos);
-router.post('/generate', authMiddleware, generateAiVideo);
-// router.post('/enhancePrompt', authMiddleware, enhancePrompt);
-// router.get('/recent', authMiddleware, getRecentVideos);
-// router.get('/all', authMiddleware, getAllVideos);
+router.get('/examples', getExampleVideos);
+// router.post('/generate', authMiddleware, generateAiVideo);
+router.post('/enhancePrompt', authMiddleware, enhancePrompt);
+router.get('/recent', authMiddleware, getRecentVideos);
+router.get('/all', authMiddleware, getAllVideos);
 
 
 module.exports = router;
