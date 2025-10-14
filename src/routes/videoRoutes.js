0c383fb15471d036ce26d066cd04d9835cb02946
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2025-10-14 16:40:58 +0530

    fix: uncomment generateAiVideo route in videoRoutes.js for functionality

--- src/routes/videoRoutes.js
+++ src/routes/videoRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
-// const { 
-//     generateAiVideo, 
+const { 
+    generateAiVideo, 
 //     getRecentVideos, 
 //     getAllVideos, 
 //     enhancePrompt, 
 //     getExampleVideos 
-// } = require('../controllers/videoController');
+} = require('../controllers/videoController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 
 // router.get('/examples', getExampleVideos);
-// router.post('/generate', authMiddleware, generateAiVideo);
+router.post('/generate', authMiddleware, generateAiVideo);
 // router.post('/enhancePrompt', authMiddleware, enhancePrompt);
 // router.get('/recent', authMiddleware, getRecentVideos);
 // router.get('/all', authMiddleware, getAllVideos);
